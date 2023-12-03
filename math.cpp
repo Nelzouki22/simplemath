7def326b19bc2e9811668b34b6663ced02fdb3e8
NADIR ELZOUKI <105569840+Nelzouki22@users.noreply.github.com>
2023-12-03 18:11:40 -0500

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::endl;
+
+void main() {
+  int i, j;
+  float x, y, z;
+  i = 5 / 2;
+  x = 5 / 2;
+  y = (float)(5 / 2);
+  j = (float)5 / 2;
+  z = 5 / 2;
+  std::cout << i < < < x << endl << y << endl << j << endl << z << endl;
+}
